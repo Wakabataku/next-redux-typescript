3011276b6f15360ee64e335b471ce0803d0275db
Wakabataku <wakaba_taku666@yahoo.co.jp>
2022-04-20 19:21:47 +0900

    create slice to increment count

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import { createSlice } from "@reduxjs/toolkit"
 
 // Reduxで管理するStateの作成
 export type CounterState = {
@@ -8,6 +9,16 @@ export type CounterState = {
 }
 // 初期値の定義
 const initialState: CounterState = { value: 0 }
+
+export const conterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.value++
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
