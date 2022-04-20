3587004b809dea56f71e6ac312a13017b55aa6c4
Wakabataku <wakaba_taku666@yahoo.co.jp>
2022-04-20 17:58:02 +0900

    create a state to be managed by Redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react"
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+
+// Reduxで管理するStateの作成
+export type CounterState = {
+  value: number
+}
+// 初期値の定義
+const initialState: CounterState = { value: 0 }
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
