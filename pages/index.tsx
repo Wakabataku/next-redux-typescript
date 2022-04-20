66153b65f6afa1a36349b0573146395dbb68d5b1
Wakabataku <wakaba_taku666@yahoo.co.jp>
2022-04-20 19:31:51 +0900

    create count component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import React from "react"
 import { NextPage } from "next"
+import { useDispatch, useSelector } from "react-redux"
+import { CounterState, counterSlice } from "./_app"
 
 const Home: NextPage = () => {
+  const dispatch = useDispatch()
+  const selector = useSelector((state: CounterState) => state)
+  const { increment } = counterSlice.actions
   return (
     <div>
-      <p>count</p>
-      <button>click</button>
+      <p>{selector.value}</p>
+      <button
+        onClick={() => {
+          dispatch(increment())
+        }}
+      >
+        click
+      </button>
     </div>
   )
 }
